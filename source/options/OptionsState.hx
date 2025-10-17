aa3d68a4179e2cbc6ad88f1521195846a70354eb
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2025-10-17 10:56:22 +0300

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -68,14 +68,13 @@ class OptionsState extends MusicBeatState
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		bg.antialiasing = ClientPrefs.data.antialiasing;
 
-		switch (ClientPrefs.themes) {
+		switch (ClientPrefs.data.themes) {
 			case 'Mods Engine':
 				bg.color = 0xFF000080;
 			
 			case 'Psych Engine':
 				bg.color = 0xFFEA71FD;
 		}
-
 
 		bg.updateHitbox();
 
