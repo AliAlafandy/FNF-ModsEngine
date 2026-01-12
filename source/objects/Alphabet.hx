e3a232ea5761cc178d0aec83c08adda86d63d5e7
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-01-12 18:55:39 +0200

    Adjust yTarget calculation and lerp behavior

--- source/objects/Alphabet.hx
+++ source/objects/Alphabet.hx
@@ -180,10 +180,10 @@ class Alphabet extends FlxSpriteGroup
 		if (isMenuItemCentered)
         {
             var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
-            var yTarget = (scaledY * yMult) + (FlxG.height * 0.48) + yAdd * 2;
+            var yTarget = (scaledY * yMult) + (FlxG.height * 0.48) + yAdd;
 
             if (Math.abs(y - yTarget) > 0.1) {
-                y = FlxMath.lerp(y, yTarget, 0.1);
+                y = FlxMath.lerp(y * 2, yTarget, 0.1);
             } else {
                 y = yTarget;
             }
