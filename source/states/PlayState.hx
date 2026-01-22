cc6d52af4584d8c3e9c4aa9ea55006655f4c4178
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-01-22 13:09:27 +0200

    Fix conditional logic for pause functionality

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1765,7 +1765,7 @@ class PlayState extends MusicBeatState
 			 #else
 			 || touchPad.buttonP.justPressed
 			 #end)
-			 && (startedCountdown && canPause))
+			 && (startedCountdown && canPause)))
 		{
 			var ret:Dynamic = callOnScripts('onPause', null, true);
 			if(ret != LuaUtils.Function_Stop) {
