712bad534ba1b51987819a67884eb4b183249f0e
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-01-22 12:35:46 +0200

    Refactor lerp value calculation for menu item

--- source/objects/Alphabet.hx
+++ source/objects/Alphabet.hx
@@ -179,14 +179,11 @@ class Alphabet extends FlxSpriteGroup
 		
 		if (isMenuItemCentered)
         {
-			var lerpVal:Float = Math.exp(-elapsed * 9.6);
-
             var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
 			var yTarget = (scaledY * yMult) + (FlxG.height * 0.48) + yAdd;
 
-            if (Math.abs(y - yTarget) > lerpVal) {
-                // y = FlxMath.lerp(y, yTarget, 0.1);
-				y = FlxMath.lerp((targetY * 1.3) + startPosition.y, y, 0.1);
+            if (Math.abs(y - yTarget) >= 0.1) {
+                y = FlxMath.lerp(y, yTarget, 0.1);
             } else {
                 y = yTarget;
             }
