618ce6f76874138f7ea318e34b3a55765b6bdddd
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-03-07 10:21:02 +0200

    Modify song text properties in FreeplayState
    Comment out menu item properties for song text.

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -109,7 +109,8 @@ class FreeplayState extends MusicBeatState
 		{
 			//var songText:Alphabet = new Alphabet(90, 320, songs[i].songName, true);
 			var songText = new Alphabet(0, 320, songs[i].songName, true);
-			songText.isMenuItemCentered = true;
+			//songText.isMenuItem = true;
+			//songText.isMenuItemCentered = true;
 			songText.targetY = i;
 			grpSongs.add(songText);
 
