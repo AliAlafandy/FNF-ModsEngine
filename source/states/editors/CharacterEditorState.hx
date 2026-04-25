3987dd03cfe94b39058aa2f710f88657867224bd
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-04-25 20:03:59 +0300

    Adjust girlfriend sprite position and scale

--- source/states/editors/CharacterEditorState.hx
+++ source/states/editors/CharacterEditorState.hx
@@ -94,11 +94,11 @@ class CharacterEditorState extends MusicBeatState
 		boyfriend.offset.set(-6, 2);
 		silhouettes.add(boyfriend);
 
-		var gf:FlxSprite = new FlxSprite(gfPosition.x, gfPosition.y).loadGraphic(Paths.image('editors/silhouetteGF'));
+		var gf:FlxSprite = new FlxSprite(gfPosition.x, gfPosition.y + 350).loadGraphic(Paths.image('editors/silhouetteGF'));
 		gf.antialiasing = ClientPrefs.data.antialiasing;
 		gf.active = false;
-		gf.offset.set(-5, 1.5);
-		gf.scale.set(3, 3);
+		// gf.offset.set(-5, 1.5);
+		gf.scale.set(2.7, 2.7);
 		silhouettes.add(gf);
 
 		silhouettes.alpha = 0.25;
