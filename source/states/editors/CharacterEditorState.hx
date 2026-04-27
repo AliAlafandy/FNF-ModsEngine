9c369e9fe517eb234916465b73aab229b10c664e
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-04-27 16:30:35 +0300

    Adjust scale of silhouette graphic in CharacterEditorState

--- source/states/editors/CharacterEditorState.hx
+++ source/states/editors/CharacterEditorState.hx
@@ -98,7 +98,7 @@ class CharacterEditorState extends MusicBeatState
 		gf.antialiasing = ClientPrefs.data.antialiasing;
 		gf.active = false;
 		// gf.offset.set(-5, 1.5);
-		gf.scale.set(2.57, 2.57);
+		gf.scale.set(2.3, 2.3);
 		silhouettes.add(gf);
 
 		silhouettes.alpha = 0.25;
