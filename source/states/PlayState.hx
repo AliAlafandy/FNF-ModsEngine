9e513d164786472128a6d725b68315222238bfec
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-05-01 09:48:05 +0300

    Fix formatting of healthBar percent comparison

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1979,7 +1979,7 @@ class PlayState extends MusicBeatState
 			case 3:
 				if (healthBar.percent < 20)
 					iconP1.animation.curAnim.curFrame = 1;
-				else if (healthBar.percent >80)
+				else if (healthBar.percent > 80)
 					iconP1.animation.curAnim.curFrame = 2;
 				else
 					iconP1.animation.curAnim.curFrame = 0;
