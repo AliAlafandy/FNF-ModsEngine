0224c4a62d22c24aa393558d0d16eac2246e7503
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-05-08 01:45:51 +0300

    Declare grid variable with var keyword

--- source/options/NotesSubState.hx
+++ source/options/NotesSubState.hx
@@ -72,7 +72,7 @@ class NotesSubState extends MusicBeatSubstate
 
 		if (ClientPrefs.data.gridTitle == true)
 		{
-			grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x33FFFFFF, 0x0));
+			var grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x33FFFFFF, 0x0));
 			grid.velocity.set(40, 40);
 			grid.alpha = 0;
 			FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
