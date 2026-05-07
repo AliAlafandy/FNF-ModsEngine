156ab78b3ecbcb5a233cf9ce7379e51a691ca3a4
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-05-08 01:40:46 +0300

    Refactor grid creation based on gridTitle preference

--- source/states/editors/ChartingState.hx
+++ source/states/editors/ChartingState.hx
@@ -240,18 +240,24 @@ class ChartingState extends MusicBeatState
 		
 		add(bg);
 
-		switch (ClientPrefs.data.themes) {
-			case 'Mods Engine':
-				grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x330000FF, 0x0));
+		if (ClientPrefs.data.gridTitle == true)
+		{
+			switch (ClientPrefs.data.themes) {
+				case 'Mods Engine':
+					grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x330000FF, 0x0));
+					grid.velocity.set(40, 40);
+					grid.alpha = 0;
+					FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
+					add(grid);
 			
-			case 'Psych Engine':
-				grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x33FFFFFF, 0x0));
+				case 'Psych Engine':
+					grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x33FFFFFF, 0x0));
+					grid.velocity.set(40, 40);
+					grid.alpha = 0;
+					FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
+					add(grid);
+			}
 		}
-
-		grid.velocity.set(40, 40);
-		grid.alpha = 0;
-		FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
-		add(grid);
 
 		gridLayer = new FlxTypedGroup<FlxSprite>();
 		add(gridLayer);
