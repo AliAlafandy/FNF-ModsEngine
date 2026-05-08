074a0b7d670b3a15480faa2efef8aa90ad5447fa
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-05-08 12:18:00 +0300

    Refactor grid creation logic in ChartingState

--- source/states/editors/ChartingState.hx
+++ source/states/editors/ChartingState.hx
@@ -242,23 +242,12 @@ class ChartingState extends MusicBeatState
 
 		if (ClientPrefs.data.lowQuality == false)
 		{
-			switch (ClientPrefs.data.themes) {
-				case 'Mods Engine':
-					grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x330000FF, 0x0));
-					grid.velocity.set(40, 40);
-					grid.scrollFactor.set(0, 0);
-					grid.alpha = 0;
-					FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
-					add(grid);
-			
-				case 'Vanilla (Normal)':
-					grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x33FFFFFF, 0x0));
-					grid.velocity.set(40, 40);
-					grid.scrollFactor.set(0, 0);
-					grid.alpha = 0;
-					FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
-					add(grid);
-			}
+			grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x33FFFFFF, 0x0));
+			grid.velocity.set(40, 40);
+			grid.scrollFactor.set(0, 0);
+			grid.alpha = 0;
+			FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
+			add(grid);
 		}
 
 		gridLayer = new FlxTypedGroup<FlxSprite>();
