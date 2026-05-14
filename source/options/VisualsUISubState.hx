899b6a40d438b2aee10c1725ba5d5beb2b6b1b1d
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-05-14 23:55:29 +0300

    Integrate FlxBackdrop and FlxGridOverlay for visuals
    Added FlxBackdrop and FlxGridOverlay for visual enhancements in the UI.

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -1,5 +1,8 @@
 package options;
 
+import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
+
 import objects.Note;
 import objects.StrumNote;
 import objects.Alphabet;
@@ -15,6 +18,15 @@ class VisualsUISubState extends BaseOptionsMenu
 	{
 		title = 'Visuals and UI';
 		rpcTitle = 'Visuals & UI Settings Menu'; //for Discord Rich Presence
+
+		if (ClientPrefs.data.lowQuality == false)
+		{
+			var grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x33FFFFFF, 0x0));
+			grid.velocity.set(40, 40);
+			grid.alpha = 0;
+			FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
+			add(grid);
+		}
 
 		// for note skins
 		notes = new FlxTypedGroup<StrumNote>();
