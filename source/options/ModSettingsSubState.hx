b1c6af0798dae994ae5d4e0ee20639a73bd45bf0
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-05-14 23:54:38 +0300

    Import FlxBackdrop and FlxGridOverlay for UI updates
    Added FlxBackdrop and FlxGridOverlay imports for visual enhancements in mod settings.

--- source/options/ModSettingsSubState.hx
+++ source/options/ModSettingsSubState.hx
@@ -1,5 +1,8 @@
 package options;
 
+import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
+
 import flixel.input.keyboard.FlxKey;
 import flixel.input.gamepad.FlxGamepadInputID;
 
@@ -17,6 +20,15 @@ class ModSettingsSubState extends BaseOptionsMenu
 		title = '';
 		//title = name;
 		rpcTitle = 'Mod Settings ($name)'; //for Discord Rich Presence
+
+		if (ClientPrefs.data.lowQuality == false)
+		{
+			var grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x33FFFFFF, 0x0));
+			grid.velocity.set(40, 40);
+			grid.alpha = 0;
+			FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
+			add(grid);
+		}
 
 		if(FlxG.save.data.modSettings == null) FlxG.save.data.modSettings = new Map<String, Dynamic>();
 		else
