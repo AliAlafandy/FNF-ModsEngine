fb1fd210634c708000d6b8eca16fd7b68e2f33ac
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-05-15 09:27:46 +0300

    Clean up GameplaySettingsSubState by removing imports
    Removed unused imports and grid backdrop logic.

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -2,24 +2,12 @@ package options;
 
 import lime.ui.Haptic;
 
-import flixel.addons.display.FlxBackdrop;
-import flixel.addons.display.FlxGridOverlay;
-
 class GameplaySettingsSubState extends BaseOptionsMenu
 {
 	public function new()
 	{
 		title = 'Gameplay Settings';
 		rpcTitle = 'Gameplay Settings Menu'; //for Discord Rich Presence
-
-		if (ClientPrefs.data.lowQuality == false)
-		{
-			var grid = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x33FFFFFF, 0x0));
-			grid.velocity.set(40, 40);
-			grid.alpha = 0;
-			FlxTween.tween(grid, {alpha: 1}, 0.5, {ease: FlxEase.quadOut});
-			add(grid);
-		}
 
 		//I'd suggest using "Downscroll" as an example for making your own option since it is the simplest here
 		var option:Option = new Option('Downscroll', //Name
