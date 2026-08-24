885d2488c433ed7650bc8e932f758b702b18bc6d
Ethantobot <ethantobot7@gmail.com>
2026-08-24 19:59:51 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -81,6 +81,10 @@ class Main extends Sprite
 		")
 		#end
 
+		#if (VIDEOS_ALLOWED && ios)
+		hxvlc.util.Handle.init(#if (hxvlc >= "1.8.0")  ['--no-lua'] #end);
+		#end
+
 		if (stage != null)
 		{
 			init();
