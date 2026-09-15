8e495e048ea3917b82689d788004f9cc4c621f8d
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-15 04:59:40 +0300

    Add FlxTransitionableState import to import.hx

--- source/import.hx
+++ source/import.hx
@@ -89,6 +89,7 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.group.FlxSpriteGroup;
 import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.addons.transition.FlxTransitionableState;
 
 import funkin.data.shaders.flixel.system.FlxShader;
 
