a268da59a6b15cc8095fe23826751b79e18474a6
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-15 04:50:52 +0300

    Replace PsychAnimationController with ModAnimationController

--- source/funkin/data/objects/Note.hx
+++ source/funkin/data/objects/Note.hx
@@ -209,7 +209,7 @@ class Note extends FlxSprite
 	{
 		super();
 
-		animation = new PsychAnimationController(this);
+		animation = new ModAnimationController(this);
 
 		antialiasing = ClientPrefs.data.antialiasing;
 		if(createdFrom == null) createdFrom = PlayState.instance;
