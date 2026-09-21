02dfc797ad1e7badda9066669e510d07fe3453c9
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-21 18:20:01 +0300

    Add modchartVideos variable if VIDEOS_ALLOWED

--- source/funkin/states/PlayState.hx
+++ source/funkin/states/PlayState.hx
@@ -114,7 +114,9 @@ class PlayState extends MusicBeatState
 	#if LUA_ALLOWED
 	public var modchartTweens:Map<String, FlxTween> = new Map<String, FlxTween>();
 	public var modchartSprites:Map<String, ModchartSprite> = new Map<String, ModchartSprite>();
+	#if VIDEOS_ALLOWED
 	public var modchartVideos:Map<String, ModchartVideo> = new Map<String, ModchartVideo>();
+	#end
 	public var modchartTimers:Map<String, FlxTimer> = new Map<String, FlxTimer>();
 	public var modchartSounds:Map<String, FlxSound> = new Map<String, FlxSound>();
 	public var modchartTexts:Map<String, FlxText> = new Map<String, FlxText>();
