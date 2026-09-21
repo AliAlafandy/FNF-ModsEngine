256e23d9d7eb02ea51c77196c5302e0b459b610a
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-21 17:43:45 +0300

    Add ModchartVideo class for video handling

---
+++ source/funkin/data/scripts/ModchartVideo.hx
@@ -0,0 +1,21 @@
+package funkin.data.scripts;
+
+#if VIDEOS_ALLOWED
+#if (hxCodec >= "3.0.0")
+import hxcodec.flixel.FlxVideoSprite as VideoSprite;
+#elseif (hxCodec >= "2.6.1")
+import hxCodec.VideoSprite as VideoSprite;
+#elseif (hxCodec == "2.6.0")
+import VideoSprite; // as VideoSprite
+#else
+import vlc.MP4Sprite as VideoSprite;
+#end
+#end
+
+class ModchartVideo extends VideoSprite
+{
+	public function new(x:Float = 0, y:Float = 0)
+	{
+		super(x, y);
+	}
+}
