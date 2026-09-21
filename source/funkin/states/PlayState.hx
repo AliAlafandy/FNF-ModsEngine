bf141f53dcdf956d8a6518406b769c9b5f219815
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-21 05:33:45 +0300

    Refactor video imports and variable types in PlayState

--- source/funkin/states/PlayState.hx
+++ source/funkin/states/PlayState.hx
@@ -40,16 +40,16 @@ import openfl.filters.ShaderFilter;
 #if VIDEOS_ALLOWED
 #if (hxCodec >= "3.0.0")
 import hxcodec.flixel.FlxVideo as VideoHandler;
-import hxcodec.flixel.FlxVideoSprite;
+import hxcodec.flixel.FlxVideoSprite as VideoSprite;
 #elseif (hxCodec >= "2.6.1")
-import hxcodec.VideoHandler; // as VideoHandler
-import hxCodec.VideoSprite as FlxVideoSprite;
+import hxcodec.VideoHandler as VideoHandler;
+import hxCodec.VideoSprite as VideoSprite;
 #elseif (hxCodec == "2.6.0")
-import VideoHandler;
-import VideoSprite as FlxVideoSprite;
+import VideoHandler; // as VideoHandler
+import VideoSprite; // as VideoSprite
 #else
 import vlc.MP4Handler as VideoHandler;
-import vlc.MP4Sprite as FlxVideoSprite;
+import vlc.MP4Sprite as VideoSprite;
 #end
 #end
 
@@ -118,7 +118,7 @@ class PlayState extends MusicBeatState
 	#if LUA_ALLOWED
 	public var modchartTweens:Map<String, FlxTween> = new Map<String, FlxTween>();
 	public var modchartSprites:Map<String, ModchartSprite> = new Map<String, ModchartSprite>();
-	public var modchartVideos:Map<String, FlxVideoSprite> = new Map<String, FlxVideoSprite>();
+	public var modchartVideos:Map<String, VideoSprite> = new Map<String, VideoSprite>();
 	public var modchartTimers:Map<String, FlxTimer> = new Map<String, FlxTimer>();
 	public var modchartSounds:Map<String, FlxSound> = new Map<String, FlxSound>();
 	public var modchartTexts:Map<String, FlxText> = new Map<String, FlxText>();
