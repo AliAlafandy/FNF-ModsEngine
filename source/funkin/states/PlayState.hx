fc9783d3c8ba5569afd8bce2e286a95303d6f5ed
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-21 03:31:26 +0300

    Add VideoSprite import for hxCodec compatibility

--- source/funkin/states/PlayState.hx
+++ source/funkin/states/PlayState.hx
@@ -40,12 +40,16 @@ import openfl.filters.ShaderFilter;
 #if VIDEOS_ALLOWED
 #if (hxCodec >= "3.0.0")
 import hxcodec.flixel.FlxVideo as VideoHandler;
+import hxcodec.flixel.FlxVideoSprite as VideoSprite;
 #elseif (hxCodec >= "2.6.1")
 import hxcodec.VideoHandler as VideoHandler;
+import hxCodec.VideoSprite as VideoSprite;
 #elseif (hxCodec == "2.6.0")
 import VideoHandler;
+import VideoSprite;
 #else
 import vlc.MP4Handler as VideoHandler;
+import vlc.MP4Sprite as VideoSprite;
 #end
 #end
 
