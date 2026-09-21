ab2706a7d7e0231a869d6255d3fdd8afeeccfbdd
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-22 00:51:20 +0300

    Fix condition checks for player music state

--- source/funkin/states/FreeplayState.hx
+++ source/funkin/states/FreeplayState.hx
@@ -272,12 +272,12 @@ class FreeplayState extends MusicBeatState
 		var shiftMult:Int = 1;
 
 		#if mobile
-		if((FlxG.keys.pressed.SHIFT || touchPad.buttonZ.pressed) && !player.playingMusic == null) shiftMult = 3;
+		if((FlxG.keys.pressed.SHIFT || touchPad.buttonZ.pressed) && !player.playingMusic) shiftMult = 3;
 		#else
-		if(FlxG.keys.pressed.SHIFT && !player.playingMusic == null) shiftMult = 3;
+		if(FlxG.keys.pressed.SHIFT && !player.playingMusic) shiftMult = 3;
 		#end
 
-		if (!player.playingMusic == null)
+		if (!player.playingMusic)
 		{
 			scoreText.text = 'PERSONAL BEST: ' + lerpScore + ' (' + ratingSplit.join('.') + '%)';
 			positionHighscore();
