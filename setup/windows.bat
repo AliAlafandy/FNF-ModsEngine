63a1b3c6266f2fc9b16e39dac0608903abd0d7fe
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-22 17:49:46 +0300

    Modify hxCodec installation command
    Updated hxCodec installation to skip dependencies.

---
+++ setup/windows.bat
@@ -0,0 +1,20 @@
+@echo off
+color 0a
+cd ..
+echo Installing dependencies...
+echo This might take a few moments depending on your internet speed.
+haxelib git lime https://github.com/AlafandyPorting/lime-0.7.3 --quiet
+haxelib install openfl 9.3.2 --quiet
+haxelib git flixel https://github.com/AliAlafandy/flixel-alafandy --quiet
+haxelib install flixel-addons 3.2.1 --quiet
+haxelib install flixel-tools 1.5.1 --quiet
+haxelib install flixel-ui 2.5.0 --quiet
+haxelib git hxcpp https://github.com/AlafandyPorting/hxcpp --quiet
+haxelib git hxCodec https://github.com/polybiusproxy/hxCodec --quiet --skip-dependencies
+haxelib git SScript https://github.com/AlafandyPorting/SScript --quiet
+haxelib install tjson 1.4.0 --quiet
+haxelib install hxdiscord_rpc --quiet --skip-dependencies
+haxelib git linc_luajit https://github.com/AlafandyPorting/linc_luajit-0.7.3 --quiet
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate.git dev --quiet
+echo Finished!
+pause
