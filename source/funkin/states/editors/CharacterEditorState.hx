809edcbf97dd5cd8fa2ce7912566e8cb1c98f1dd
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-22 19:55:04 +0300

    Add conditional background color for editor state

--- source/funkin/states/editors/CharacterEditorState.hx
+++ source/funkin/states/editors/CharacterEditorState.hx
@@ -1175,6 +1175,9 @@ class CharacterEditorState extends MusicBeatState
 		/////////////
 		// bg data //
 		/////////////
+		#if !BASE_GAME_FILES
+		camEditor.bgColor = 0xFF666666;
+		#else
 		var bg:BGSprite = new BGSprite('stageback', -600, -200, 0.9, 0.9);
 		add(bg);
 
@@ -1182,6 +1185,7 @@ class CharacterEditorState extends MusicBeatState
 		stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
 		stageFront.updateHitbox();
 		add(stageFront);
+		#end
 
 		dadPosition.set(100, 100);
 		bfPosition.set(770, 100);
